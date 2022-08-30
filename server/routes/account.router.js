cb62b7ab2428795f3ed210a18c7e2439ea2e62d9
Mike Flintrop <mike.flintrop@gmail.com>
2022-08-30 11:46:19 -0500

    updated router

--- server/routes/account.router.js
+++ server/routes/account.router.js
@@ -4,7 +4,24 @@ const router = express.Router();
 const pool = require('../modules/pool');
 
 router.get('/', (req, res) => {
-  res.send('Hello?');
+  // res.send('Hello?');
+  const sqlText = `
+    SELECT "account"."name", SUM("register"."amount")
+    FROM "account"
+    JOIN "register" ON "account"."id" = "register"."acct_id"
+    GROUP BY "account"."name" 
+    ORDER BY "account"."name";
+  `;
+  pool.query(sqlText)
+    .then(result => {
+      console.log('Money numbers:', result.rows);
+      res.send(result.rows);
+    })
+    .catch(error => {
+      console.log('Error moving money:', error);
+      res.sendStatus(500)
+    });
 })
+
 
 module.exports = router;
